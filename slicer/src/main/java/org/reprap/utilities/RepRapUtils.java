a8b09ea309ea7fe8fd02e14f7e4be3ae70785c02
Holger Oehm <holger.oehm@holger-oehm.de>
2013-04-14 18:30:46 +0200

    removed unreferenced stuff

--- slicer/src/main/java/org/reprap/utilities/RepRapUtils.java
+++ slicer/src/main/java/org/reprap/utilities/RepRapUtils.java
@@ -2,7 +2,6 @@ package org.reprap.utilities;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -10,7 +9,6 @@ import java.io.LineNumberReader;
 import java.net.JarURLConnection;
 import java.net.URI;
 import java.net.URL;
-import java.nio.channels.FileChannel;
 import java.util.Collections;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
@@ -19,38 +17,6 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 
 public class RepRapUtils {
-    /**
-     * Copy a file from one place to another
-     */
-    public static void copyFile(final File src, final File dest) throws IOException {
-        if (!dest.exists()) {
-            dest.createNewFile();
-        }
-
-        FileChannel source = null;
-        FileChannel destination = null;
-
-        try {
-            source = new FileInputStream(src).getChannel();
-            destination = new FileOutputStream(dest).getChannel();
-            destination.transferFrom(source, 0, source.size());
-        } finally {
-            if (source != null) {
-                source.close();
-            }
-            if (destination != null) {
-                destination.close();
-            }
-        }
-    }
-
-    /**
-     * Copy a directory tree from one place to another
-     */
-    public static void copyTree(final File src, final File dest) throws IOException {
-        FileUtils.copyDirectory(src, dest);
-    }
-
     public static int countLines(final File f) throws IOException {
         final LineNumberReader reader = new LineNumberReader(new InputStreamReader(new FileInputStream(f)));
         try {
@@ -65,7 +31,7 @@ public class RepRapUtils {
     public static void copyResourceTree(final URL source, final File target) throws IOException {
         switch (source.getProtocol()) {
         case "file":
-            copyTree(toFile(source), target);
+            FileUtils.copyDirectory(toFile(source), target);
             break;
         case "jar":
             copyJarTree(source, target);
